2b92481f14c8413343bbc3393af357a05b6b2ecd
Joaquín <jncaceres@uc.cl>
2022-04-17 14:37:21 -0400

    use functional components

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,13 +1,12 @@
 import React from "react"
 import PropTypes from "prop-types"
-class HelloWorld extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-        Greeting: {this.props.greeting}
-      </React.Fragment>
-    );
-  }
+
+const HelloWorld = (props) => {
+  return (
+    <React.Fragment>
+        Greeting: {props.greeting}
+    </React.Fragment>
+  )
 }
 
 HelloWorld.propTypes = {

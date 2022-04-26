100ed9181846f2952e4428ee12aed4fe7f110d16
Joaquín <jncaceres@uc.cl>
2022-04-26 01:42:41 -0400

    fix: change test name

--- test/javascript/components/Home.spec.js
+++ test/javascript/components/Home.spec.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Home from '../../../app/javascript/components/Home'
 
-it('Hello test', () => {
+it('Home test', () => {
   const { getByText } = render(<Home />);
   expect(getByText('Encuentra viajes en auto, a precio barato')).toBeInTheDocument();
 });

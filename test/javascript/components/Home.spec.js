e4b77f5b3d2c226a44aa012b428444c9163ae2f2
Joaquín <jncaceres@uc.cl>
2022-04-26 01:38:50 -0400

    test: add first jest test

---
+++ test/javascript/components/Home.spec.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Home from '../../../app/javascript/components/Home'
+
+it('Hello test', () => {
+  const { getByText } = render(<Home />);
+  expect(getByText('Encuentra viajes en auto, a precio barato')).toBeInTheDocument();
+});

80129d661b98877b2c52e6efdbb16c2ad319aef7
mjespinoza <mjespinoza2@uc.cl>
2022-04-29 23:46:42 -0400

    fix/select gender tag

--- app/javascript/components/devise/auth/sign-upForm.js
+++ app/javascript/components/devise/auth/sign-upForm.js
@@ -1,73 +1,60 @@
 import React from "react";
+
 //import background from "../../../assets/background.jpg";
 
 const registrationForm = (props) => {
     return (
         <form action = {props.registration_path} method= "post" acceptCharset="UTF-8">
-        <div className="field">
+          <div className="field">
             <div className="control">
-                <input type="hidden" name="authenticity_token" value={document.getElementsByName("csrf-token")[0].getAttribute("content")} autoComplete="off"></input>
+                <input 
+                type="hidden" 
+                name="authenticity_token" 
+                value={document.getElementsByName("csrf-token")[0].getAttribute("content")}
+                autoComplete="off">
+                </input>
             </div>
-        </div>
-          <input
-          type='text'
-          name='name'
-          placeholder='Nombre'
-          required
-          />
-          <input
-          type='text'
-          name='last_name'
-          placeholder='Apellido'
-          required
-          />
-          <input
-          type='rut'
-          name='rut'
-          placeholder='RUT'
-          required
-          />
-          <input
-          type='text'
-          name='phone'
-          placeholder='Telefono'
-          required
-          />
-          <select
-          name='gender'
-          placeholder='Género'
-          />
-          <option value = "Femenino">Femenino</option>
-          <option value ="Masculino">Masculino</option>
-          <input
-          type='date'
-          name='birthdate'
-          placeholder='Fecha de cumpleaños'
-          required
-          />
-          <input
-          type='email'
-          name='email'
-          placeholder='Email'
-          required
-          />
-
-          <input
-          type='password'
-          name='password'
-          placeholder='Contraseña'
-          required
-          />
-
-          <input
-          type='password'
-          name='password_confirmation'
-          placeholder='Confirmación de contraseña'
-          required
-          />
-
-          <button type='submit'>Confirmar</button>
           
+          </div>
+          <div className="field">
+            <input type='text' name='name' placeholder='Nombre' required/>
+          </div>
+          <div className="field">
+            <input type='text' name='last_name' placeholder='Apellido' required/>
+          </div> 
+          <div className="field">
+            <input type='rut' name='rut' placeholder='RUT' required/>
+          </div>
+          <div>
+            <input type='text' name='phone' placeholder='Telefono' required/>
+          </div>
+          <div className="field">
+            <select required>    
+              <option value= "F">Femenino</option>
+              <option value= "M">Masculino</option>
+            </select>
+          </div>
+          <div className="field">
+            <input type='date' name='birthdate' placeholder='Fecha de cumpleaños' required/>
+          </div>
+          <div className="field">
+            <input type='email' name='email' placeholder='Email' required/>
+          </div>
+          <div className="field">
+            <input type='password' name='password' placeholder='Contraseña' required/>
+
+          </div>
+          <div className="field">
+            <input type='password' name='password_confirmation' placeholder='Confirmar contraseña' required/>
+          </div>
+          <div className="field">
+           <div className="control">
+             <button type='submit'>Confirmar</button>
+           </div>
+            
+          </div>
+
+      
         </form>
       );
 }

2ebeb8c74fe7861f651615cb9520574592e1a8f5
burgosibarra <burgosibarra@uc.cl>
2022-05-19 21:53:10 -0400

    fix: rubocop was aplied

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :user do
     name { 'Some Name' }
     last_name { 'Some LastName' }
-    email { ('AAAAA'..'ZZZZZ').to_a.sample + rand(100...900).to_s + "@yopmail.com" }
+    email { "#{('AAAAA'..'ZZZZZ').to_a.sample}#{rand(100...900)}@yopmail.com" }
     rut { '1111111-1' }
     phone { '12345678' }
     birthdate { '2000-02-02' }

c234f8c28ed3a9c9856879ace7ed1717ac09dc22
Joaquín <jncaceres@uc.cl>
2022-05-21 03:36:17 -0400

    chore: resolve conflicts

--- test/javascript/components/Home.spec.js
+++ test/javascript/components/Home.spec.js
@@ -3,7 +3,18 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Home from '../../../app/javascript/components/Home'
 
+const places = [
+  {
+    id: 1,
+    name: 'RM'
+  },
+  {
+    id: 2,
+    name: 'Iquique'
+  }
+]
+
 it('Home test', () => {
-  const { getByText } = render(<Home />);
+  const { getByText } = render(<Home places={places} />);
   expect(getByText('Encuentra viajes en auto, a precio barato')).toBeInTheDocument();
 });

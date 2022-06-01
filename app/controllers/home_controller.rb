ea78acbcb1835462c8006db69c57d473fc292b95
Joaquín <jncaceres@uc.cl>
2022-05-31 21:24:53 -0400

    doc: add yard docs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  # Obtiene todos los lugares (Places) para viajar.
+  #
+  # @return [Array<Place>] 
   def index
     @places = Place.all
   end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,61 @@
 # frozen_string_literal: true
 
+# {Trip} modelo responsable de almacenar la información de los viajes.
+#
+# @!attribute id
+#   @return [Int] ID de {Trip}.
+#
+# @!attribute from_address
+#   @return [String] Dirección de salida de {Trip}.
+#
+# @!attribute to_address
+#   @return [String] Dirección de llegada de {Trip}.
+#
+# @!attribute available_seats
+#   @return [Int] Asientos disponibles de {Trip}.
+#
+# @!attribute leaving_at
+#   @return [DateTime] Momento de salida de {Trip}.
+#
+# @!attribute price
+#   @return [Int] Precio de {Trip}.
+#
+# @!attribute comments
+#   @return [String] Comentario de {Trip}.
+#
+# @!attribute car_license_plate
+#   @return [String] Patente del auto de {Trip}.
+#
+# @!attribute car_brand
+#   @return [String] Marca del auto de {Trip}.
+#
+# @!attribute car_model
+#   @return [String] Modelo del auto de {Trip}.
+#
+# @!attribute car_color
+#   @return [String] Color del auto de {Trip}.
+#
+# @!attribute user
+#   @return [User] Usuario creador del {Trip}.
+#
+# @!attribute passenger_requests
+#   @return [Array<PassengerRequest>] Solicitudes de {Trip}.
+#
+# @!attribute to
+#   @return [Place] Lugar de llegada de {Trip}.
+#
+# @!attribute from
+#   @return [Place] Lugar de salida de {Trip}.
+#
+# @!attribute users
+#   @return [Array<User>] Usuarios pasajeros del {Trip}.
+#
+# @!attribute created_at
+#   @return [DateTime] Momento en que {Trip} fue creado.
+#
+# @!attribute updated_at
+#   @return [DateTime] Momento en que {Trip} fue editado por ultima vez.
+
 class Trip < ApplicationRecord
   belongs_to :user
   has_many :passenger_requests
@@ -20,6 +76,7 @@ class Trip < ApplicationRecord
   validates :from_id, presence: { message: 'Ingresar la comuna de origen' }
   validates :to_id, presence: { message: 'Ingresar la comuna de destino' }
 
+  # retorna si es que la salida de un nuevo viaje es valida o no.
   def check_trip_leaving_at
     errors.add(:leaving_at, 'Error en la fecha del viaje') if leaving_at && (leaving_at.to_date < Date.today)
   end

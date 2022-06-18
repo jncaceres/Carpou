411af5bad58a05595ca82aa7824a18ddac9aff01
Joaquín <jncaceres@uc.cl>
2022-06-18 00:02:04 -0400

    fix: rubocop fixes

--- app/admin/trips.rb
+++ app/admin/trips.rb
@@ -1,18 +1,20 @@
-ActiveAdmin.register Trip do
+# frozen_string_literal: true
 
+ActiveAdmin.register(Trip) do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :from_address, :to_address, :available_seats, :leaving_at, :price, :comments, :car_license_plate, :car_brand, :car_model, :car_color, :user_id, :from_id, :to_id
+  # permit_params :from_address, :to_address, :available_seats, :leaving_at, :price,
+  #               :comments, :car_license_plate, :car_brand, :car_model, :car_color, :user_id, :from_id, :to_id
   #
   # or
   #
   # permit_params do
-  #   permitted = [:from_address, :to_address, :available_seats, :leaving_at, :price, :comments, :car_license_plate, :car_brand, :car_model, :car_color, :user_id, :from_id, :to_id]
+  #   permitted = [:from_address, :to_address, :available_seats, :leaving_at, :price, :comments, :car_license_plate, :car_brand,
+  #                :car_model, :car_color, :user_id, :from_id, :to_id]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
 end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 ActiveAdmin.register(User) do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
@@ -17,12 +19,16 @@ ActiveAdmin.register(User) do
     column :email
     actions
   end
-  permit_params :name, :last_name, :rut, :phone, :gender, :birthdate, :admin, :email # , :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :failed_attempts, :unlock_token, :locked_at
+  permit_params :name, :last_name, :rut, :phone, :gender, :birthdate, :admin, :email
+  # , :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at,
+  #   :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :failed_attempts, :unlock_token, :locked_at
   #
   # or
   #
   # permit_params do
-  #   permitted = [:name, :last_name, :rut, :phone, :gender, :birthdate, :admin, :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :failed_attempts, :unlock_token, :locked_at]
+  #   permitted = [:name, :last_name, :rut, :phone, :gender, :birthdate, :admin, :email, :encrypted_password,
+  #                :reset_password_token, :reset_password_sent_at, :remember_created_at, :confirmation_token,
+  #                :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :failed_attempts, :unlock_token, :locked_at]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end

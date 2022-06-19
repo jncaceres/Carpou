7e977366082cd25cf27b544ba6c1e4b89e10951a
María José Varela <42159497+mjvarela@users.noreply.github.com>
2022-06-19 12:08:56 -0400

    refactor create and edit trip form

--- app/javascript/components/TripForm.js
+++ app/javascript/components/TripForm.js
@@ -1,84 +1,134 @@
-import React, { useState } from "react";
-import Select from "react-select";
+import React from "react";
 import { routes } from "../api";
 
 const TripForm = (props) => {
-  const { places } = props;
-  const options = places.map((place) => ({
-    value: place.id,
-    label: place.name,
-  }));
-  const [from, setFrom] = useState(options[0]);
-  const [to, setTo] = useState(options[options.length - 1]);
-  const [date, setDate] = useState(new Date().toLocaleDateString("en-CA"));
 
-  const handleFromChange = (e) => {
-    setFrom(e);
-  };
+    const { user,  places, trip } = props;
+    return (
+        <form action = {trip ? routes.trips.update(trip.id) : routes.trips.post()}
+            method="post"
+            acceptCharset="UTF-8" 
+            style={{border: "1px solid #f5efef", padding: 20,
+            borderRadius: 10, backgroundColor: "#f5efef"}}>
+          <div className="field">
+            <div className="control">
+                <input 
+                    type="hidden" 
+                    name="authenticity_token" 
+                    value={document.getElementsByName("csrf-token")[0].getAttribute("content")}
+                    autoComplete="off">
+                </input>
+            </div>      
+          </div>
+          <div className="field is-horizontal">
+            <div className="field-body">
+              <div className="field is-expanded">
+                <label className="label">Viajo desde...</label>
+                <div className="control">
+                  <select className="input" name='trip[from_id]' defaultValue={trip?.from_id}>
+                  {places.map((place) => (
+                    <option 
+                        key={place.id} 
+                        value={place.id}>{place.name} 
+                    </option> 
+                    ))}
+                  </select>
+                </div>
+              </div>
+              <div className="field is-expanded">
+                <label className="label">Viajo hacia...</label>
+                <div className="control">
+                  <select className="input" name = 'trip[to_id]' defaultValue={trip?.to_id}>
+                    {places.map((place) => (
+                        <option 
+                            key={place.id} 
+                            value={place.id}>{place.name} 
+                        </option> 
+                    ))}
+                    </select>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="field is-horizontal">
+            <div className="field-body">
+              <div className="field is-expanded">
+                <div className="control">
+                  <input className="input" type='text' name='trip[from_address]' placeholder='Dirección de origen' defaultValue={trip?.from_address} required/>
+                </div>
+              </div>
+              <div className="field is-expanded">
+                <div className="control">
+                  <input className="input" type='text' name='trip[to_address]' placeholder='Dirección de destino' defaultValue={trip?.from_address} required/>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="field is-horizontal">
+            <div className="field-body">
+              <div className="field is-expanded">
+                <div className="control">
+                  <input className="input" type='number' name='trip[available_seats]' placeholder='Número de asientos disponibles' defaultValue={trip?.available_seats} required/>
+                </div>
+              </div>
+              <div className="field is-expanded">
+                <div className="control">
+                  <input className="input" type='text' name='trip[price]' placeholder='Monto a pagar' defaultValue={trip?.price} required/>
+                </div>
+              </div>
+              <div className="field is-expanded">
+                <div className="control">
+                  <input className="input" type='datetime-local' name='trip[leaving_at]' placeholder='Hora de salida' defaultValue={trip?.leaving_at} required/>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="field is-horizontal">
+            <label className="label">Información del auto</label>
+            <div className="field-body">
+              <div className="field is-expanded">
+                <div className="control">
+                  <input className="input" type='text' name='trip[car_license_plate]' placeholder='Patente' defaultValue={trip?.car_license_plate} required/>
+                </div>
+              </div>
+              <div className="field is-expanded">
+                <div className="control">
+                  <input className="input" type='text' name='trip[car_brand]' placeholder='Marca del auto' defaultValue={trip?.car_brand} required/>
+                </div>
+              </div>
+              <div className="field is-expanded">
+                <div className="control">
+                  <input className="input" type='text' name='trip[car_model]' placeholder='Modelo del auto' defaultValue={trip?.car_model} required/>
+                </div>
+              </div>
+              <div className="field is-expanded">
+                <div className="control">
+                  <input className="input" type='text' name='trip[car_color]' placeholder='Color del auto' defaultValue={trip?.car_color} required/>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="field">
+            <div className="control">
+                <input 
+                type="hidden" 
+                name='trip[user_id]' 
+                value={user.id}>
+                </input>
+            </div>      
+          </div>
+          <div className="field">
+            <div className="control">
+              <input className="input" type='text' name='trip[comments]' placeholder='Comentario' defaultValue={trip?.comments} required/>
+            </div>
+          </div>
+          <div className="field">
+           <div className="control">
+             <button className="button is-primary is-fullwidth" type='submit'>{trip ? 'Editar viaje' : 'Crear viaje'}</button>
+           </div>           
+          </div>
+      </form>
+    )
 
-  const handleToChange = (e) => {
-    setTo(e);
-  };
-
-  const handleDateChange = (e) => {
-    setDate(e.target.value);
-  };
-
-  const tripParams = { from: from.value, to: to.value, date: date };
-
-  return (
-    <div
-      style={{
-        border: "1px solid #f5efef",
-        padding: 10,
-        borderRadius: 10,
-        backgroundColor: "#ffff",
-      }}
-    >
-      <div className="field">
-        <div className="control">
-          <Select
-            options={options}
-            name="from"
-            placeholder="Viajo desde..."
-            value={from}
-            onChange={handleFromChange}
-          />
-        </div>
-      </div>
-      <div className="field">
-        <div className="control">
-          <Select
-            options={options}
-            name="to"
-            placeholder="Viajo hacia..."
-            value={to}
-            onChange={handleToChange}
-          />
-        </div>
-      </div>
-      <div className="field">
-        <div className="control">
-          <input
-            className="input"
-            type="date"
-            placeholder="Hoy"
-            min={new Date().toLocaleDateString("en-CA")}
-            value={date}
-            onChange={handleDateChange}
-          />
-        </div>
-      </div>
-      <div className="field">
-        <div className="control">
-          <a href={routes.trips.index(tripParams)}>
-            <button className="button is-primary is-fullwidth" type="submit">
-              Buscar viajes
-            </button>
-          </a>
-        </div>
-      </div>
-    </div>
-  );
-};
+}
 export default TripForm;

bbeb7456634289dc02f41b1c45cc2efa923d7018
mjespinoza <mjespinoza2@uc.cl>
2022-06-20 17:46:54 -0400

    handleChange

--- app/javascript/components/newTrip.js
+++ app/javascript/components/newTrip.js
@@ -1,24 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { routes } from "../api";
+import Select from "react-select";
 const newTrip = (props) =>{
   const {user,  places,notice} = props;
+  const options = places.map((place) => ({
+    value: place.id,
+    label: place.name,
+  }));
+  const [inputValues, setInputValues] = useState({
+    authenticity_token: document.getElementsByName("csrf-token")[0].getAttribute("content"),
+    trip:{
+      from_id: options[0],
+      to_id: options[options.length - 1],
+      from_address: '',
+      to_adrress: '',
+      available_seats: '',
+      leaving_at:'',
+      price:'',
+      comments: '',
+      car_license_plate: '',
+      car_brand:'',
+      car_model:'',
+      car_color: '',
+      user_id: user.id
+    }
+  });
+
+  const handleChange = (e) => {
+    console.log(e.target);
+    console.log(inputValues);
+    const { name, value } = e.target;
+    console.log(name, value);
+    setInputValues({ ...inputValues, [name]: value });
+    console.log(inputValues);
+
+  }
+  const sendData = (data) =>{
+    fetch(routes.trips.post(),{
+      method:"POST",
+      body: data
+    })
+
+  }  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  }
 
   return (
-      <form action = {routes.trips.post()}
-      method="post"
+      <form 
+      onSubmit={handleSubmit}
       acceptCharset="UTF-8" 
       style={{border: "1px solid #f5efef", padding: 5,
       borderRadius: 10, backgroundColor: "#ffff"}}>
-          <div className="field">
-            <div className="control">
-                <input 
-                type="hidden" 
-                name="authenticity_token" 
-                value={document.getElementsByName("csrf-token")[0].getAttribute("content")}
-                autoComplete="off">
-                </input>
-            </div>      
-          </div>
           <div className="field">
             <div className="control">
               <p>{notice}</p>
@@ -26,92 +60,133 @@ const newTrip = (props) =>{
           </div>
           <div className="field">
             <div className="control">
-              <input className="input" type='text' name='trip[from_address]' placeholder='Dirección de origen' required/>
+              <Select
+                options={options}
+                name="from_id"
+                placeholder="Viajo desde..."
+                value={inputValues.trip.from_id}
+                onChange={handleChange}
+              />
             </div>
           </div>
           <div className="field">
             <div className="control">
-              <input className="input" type='text' name='trip[to_address]' placeholder='Dirección de destino' required/>
+              <input className="input" type='text' 
+              name='from_address' 
+              placeholder='Dirección de origen'
+              onChange={handleChange}
+              required/>
             </div>
           </div>
           <div className="field">
             <div className="control">
-              <input className="input" type='number' name='trip[available_seats]' placeholder='Número de asientos disponibles' required/>
+              <Select
+                options={options}
+                name="to_id"
+                placeholder="Viajo hacia..."
+                value={inputValues.trip.to_id}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          
+          <div className="field">
+            <div className="control">
+              <input className="input" 
+              type='text' 
+              name='to_address' 
+              placeholder='Dirección de destino' 
+              onChange={handleChange}
+              required/>
             </div>
           </div>
           <div className="field">
             <div className="control">
-              <input className="input" type='datetime-local' name='trip[leaving_at]' placeholder='Hora de salida' required/>
+              <input className="input" 
+              type='number' 
+              name='available_seats' 
+              placeholder='Número de asientos disponibles' 
+              onChange={handleChange}
+              required/>
             </div>
           </div>
           <div className="field">
             <div className="control">
-              <input className="input" type='text' name='trip[price]' placeholder='Monto a pagar' required/>
+              <input className="input" 
+              type='datetime-local' 
+              name='leaving_at' 
+              placeholder='Hora de salida' 
+              onChange={handleChange}
+              required/>
             </div>
           </div>
           <div className="field">
             <div className="control">
-              <input className="input" type='text' name='trip[comments]' placeholder='Comentario' required/>
+              <input className="input" 
+                type='text' 
+                name='price' 
+                placeholder='Monto a pagar' 
+                onChange={handleChange}
+                required/>
             </div>
           </div>
           <div className="field">
             <div className="control">
-              <input className="input" type='text' name='trip[car_license_plate]' placeholder='Patente' required/>
+              <input className="input" 
+              type='text' 
+              name='comments' 
+              placeholder='Comentario' 
+              onChange={handleChange}
+              required/>
             </div>
           </div>
           <div className="field">
             <div className="control">
-              <input className="input" type='text' name='trip[car_brand]' placeholder='Marca del auto' required/>
+              <input className="input" 
+              type='text' 
+              name='car_license_plate' 
+              placeholder='Patente' 
+              onChange={handleChange}
+              required/>
             </div>
           </div>
           <div className="field">
             <div className="control">
-              <input className="input" type='text' name='trip[car_model]' placeholder='Modelo del auto' required/>
+              <input className="input" 
+              type='text' 
+              name='car_brand' 
+              placeholder='Marca del auto' 
+              onChange={handleChange}
+              required/>
             </div>
           </div>
           <div className="field">
             <div className="control">
-              <input className="input" type='text' name='trip[car_color]' placeholder='Color del auto' required/>
+              <input className="input" 
+              type='text' 
+              name='car_model' 
+              placeholder='Modelo del auto'
+              onChange={handleChange} 
+              required/>
             </div>
           </div>
           <div className="field">
             <div className="control">
-                <input 
-                type="hidden" 
-                name='trip[user_id]' 
-                value={user.id}>
-                </input>
-  
-
-            </div>      
-          </div>
-          <div className="field">
-            <div className="control">
-              <label>Viajo desde...</label>
-              <select className="input" name='trip[from_id]'>
-                {places.map((option)=>(
-                  // eslint-disable-next-line react/jsx-key
-                  <option value={option.id}>{option.name}</option>
-                ))}
-              </select>
-            </div>
-          </div>
-          <div className="field">
-            <div className="control">
-              <label>Viajo hacia...</label>
-            <select className="input" name = 'trip[to_id]'>
-                {places.map((option)=>(
-                  // eslint-disable-next-line react/jsx-key
-                  <option value={option.id}>{option.name}</option>
-                ))}
-              </select>
+              <input className="input" 
+              type='text' 
+              name='car_color' 
+              placeholder='Color del auto' 
+              onChange={handleChange}
+              required/>
             </div>
           </div>
           <div className="field">
            <div className="control">
-             <button className="button is-primary is-fullwidth" type='submit'>Crear viaje</button>
+             <button className="button is-primary is-fullwidth" 
+             type='submit'>Crear viaje</button>
            </div>           
           </div>
+         
       </form>
 
 

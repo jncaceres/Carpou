518f27cd8d371e63f4972dfe984104dd2e7cb56e
Joaquín <jncaceres@uc.cl>
2022-06-20 13:37:41 -0400

    fix: fix coflicts

--- app/javascript/components/TripShow.js
+++ app/javascript/components/TripShow.js
@@ -11,37 +11,37 @@ const TripShow = (props) => {
 
   return (
     <div className="container">
-      <br/>
+      <br />
       {user && user.id == trip.user.id ? (
         <>
           <div className="columns container is-mobile">
             <div className="column is-full-mobile ">
-              <div className="title">
-                Detalle de mi viaje
-              </div>
+              <div className="title">Detalle de mi viaje</div>
               <div className="subtitle mb-1">
                 Viaje desde {`${trip.from.name} a ${trip.to.name}`}
               </div>
             </div>
             <div className="column is-full-mobile is-one-third-tablet is-one-third-desktop is-one-quarter-widescreen is-one-quarter-fullhd">
-              <TripButton buttonText="Editar viaje" route={routes.trips.edit(trip.id)} />
-            </div>
-          </div>
-          <div className="columns is-centered block">
-            <div className="column is-full-mobile">
-              <TripData trip={trip} showUser={false} />
+              <TripButton
+                buttonText="Editar viaje"
+                route={routes.trips.edit(trip.id)}
+              />
+              <DeleteTripButton
+                buttonText="Cancelar viaje"
+                route={routes.trips.delete(trip.id)}
+              />
             </div>
           </div>
           <TripData trip={trip} showUser={false} />
-          <TripButton buttonText="Editar viaje" route={routes.trips.edit(trip.id)} />
-          <DeleteTripButton buttonText="Cancelar viaje" route={routes.trips.delete(trip.id)} />
           <TripPassengerRequests passengerRequests={passengerRequests} />
         </>
       ) : (
         <>
           <div className="columns is-multiline is-centered">
             <div className="column has-text-centered is-full-mobile is-full-tablet is-full-desktop is-three-quarters-widescreen is-two-thirds-fullhd mb-1">
-              <h1 className="title block">Viaje desde {`${trip.from.name} a ${trip.to.name}`}</h1>
+              <h1 className="title block">
+                Viaje desde {`${trip.from.name} a ${trip.to.name}`}
+              </h1>
               <TripMap trip={trip} height={350} />
             </div>
             <div className="column is-full-mobile is-full-tablet is-full-desktop is-three-quarters-widescreen is-two-thirds-fullhd">
@@ -49,16 +49,11 @@ const TripShow = (props) => {
               <div className="block">
                 <TripButton
                   buttonText="Solicitar unirme al viaje"
-                  route={routes.passenger_requests.new({trip_id: trip.id})}
+                  route={routes.passenger_requests.new({ trip_id: trip.id })}
                 />
               </div>
             </div>
           </div>
-          <TripData trip={trip} />
-          <TripButton
-            buttonText="Solicitar unirme al viaje"
-            route={routes.passenger_requests.new({trip_id: trip.id})}
-          />
         </>
       )}
     </div>

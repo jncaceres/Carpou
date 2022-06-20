5ae7680d199380635b24ba37636968a38198da6d
Joaquín <jncaceres@uc.cl>
2022-06-20 01:06:21 -0400

    fix: move code

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,9 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-Rack::MiniProfiler.config.position = 'bottom-right'
-
-
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.

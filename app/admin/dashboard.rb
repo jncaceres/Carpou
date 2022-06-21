7f55947d7c11762204b39f45391c0ca162008c77
Bastian Ceppi <bceppi@ordename.cl>
2022-06-21 16:12:33 -0400

    fix: linter fixes

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -6,7 +6,7 @@ ActiveAdmin.register_page('Dashboard') do
   content title: proc { I18n.t('active_admin.dashboard') } do
     div class: 'blank_slate_container', id: 'dashboard_default_message' do
       span class: 'blank_slate' do
-        span "Bienvenid@!"
+        span 'Bienvenid@!'
         # small I18n.t('active_admin.dashboard_welcome.call_to_action')
       end
     end
@@ -28,7 +28,9 @@ ActiveAdmin.register_page('Dashboard') do
         panel 'Solicitudes de viaje recientes' do
           ul do
             PassengerRequest.last(15).map do |pr|
-              li link_to("#{pr.user.email} solicita viaje de #{pr.trip.from.name} a #{pr.trip.to.name} por #{pr.trip.user.email}", admin_passenger_request_path(pr))
+              li link_to("#{pr.user.email} solicita viaje de #{pr.trip.from.name} a #{pr.trip.to.name} por #{pr.trip.user.email}",
+                         admin_passenger_request_path(pr)
+                        )
             end
           end
         end

1f415becd14576d11b87ee597c39b71078f9e871
Bastian Ceppi <bceppi@ordename.cl>
2022-06-21 15:51:33 -0400

    feat: admin customization of PassengerRequest

--- app/admin/passenger_requests.rb
+++ app/admin/passenger_requests.rb
@@ -6,7 +6,7 @@ ActiveAdmin.register(PassengerRequest) do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :comments, :status, :user_id, :trip_id
+  permit_params :comments, :status, :user_id, :trip_id
   #
   # or
   #
@@ -15,4 +15,26 @@ ActiveAdmin.register(PassengerRequest) do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+
+  sidebar 'Asociaciones', only: %i[show edit] do
+    ul do
+      li link_to('Usuario', admin_user_path(resource.user))
+      li link_to('Viaje', admin_trip_path(resource.trip))
+    end
+  end
+
+  form title: 'Editar Passenger Request' do |f|
+    inputs do
+      li "ID: #{f.object.id}" unless f.object.new_record?
+      input :user_id, as: :select, collection: User.all.map { |u| ["#{u.name} #{u.last_name} #{u.email}", u.id] }, include_blank: false
+      input :trip_id, as: :select, collection: Trip.eager_load(:from, :to, :user).all.map { |t|
+                                                 ["#{t.from.name}/#{t.to.name} de #{t.user.email}", t.id]
+                                               }, include_blank: false
+      input :status, include_blank: false
+      input :comments
+      li "Created at #{f.object.created_at}" unless f.object.new_record?
+    end
+    para 'Press cancel to return to the list without saving.'
+    actions
+  end
 end
